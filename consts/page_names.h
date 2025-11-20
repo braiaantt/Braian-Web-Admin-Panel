6affa0ffaac8498fb6f005d58e10939fa8e21b98
Braian Torres <brianntorress.s@gmail.com>
2025-11-20 16:14:32 -0300

    chore(consts): add PageName consts

---
+++ consts/page_names.h
@@ -0,0 +1,12 @@
+#ifndef PAGE_NAMES_H
+#define PAGE_NAMES_H
+
+#include <QString>
+
+namespace PageName {
+    inline const QString LOGIN = "loginPage";
+    inline const QString PORTFOLIO = "portfolioPage";
+    inline const QString PROJECT = "projectPage";
+}
+
+#endif // PAGE_NAMES_H
