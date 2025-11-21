b8e845a2012ab927ac91bc965e21bd8264782142
Braian Torres <brianntorress.s@gmail.com>
2025-11-21 16:37:40 -0300

    refactor(TechnologyWidget): add Technology paramter to constructor and call init() method at constructor feat: implement init() method

--- custome_widgets/technologywidget.cpp
+++ custome_widgets/technologywidget.cpp
@@ -1,17 +1,28 @@
-#include "technology.h"
-#include "ui_technology.h"
+#include "technologywidget.h"
+#include "ui_technologywidget.h"
+#include "utils.h"
 
-Technology::Technology(QWidget *parent, const QPixmap &photo, const QString &name, const QString &_filePath)
+TechnologyWidget::TechnologyWidget(QWidget *parent, const Technology &technology)
     : QWidget(parent)
     , ui(new Ui::Technology)
 {
     ui->setupUi(this);
-    ui->labelPhoto->setPixmap(photo);
-    ui->labelName->setText(name);
-    filePath = _filePath;
+    init(technology);
 }
 
-Technology::~Technology()
+TechnologyWidget::~TechnologyWidget()
 {
     delete ui;
+}
+
+//------ Initialization ------
+
+void TechnologyWidget::init(const Technology &technology)
+{
+    if(!technology.getImgPath().isEmpty()){
+        QPixmap pixmap = Utils::roundedPixmap(QPixmap(technology.getImgPath()), ui->labelPhoto->size());
+        ui->labelPhoto->setPixmap(pixmap);
+    }
+
+    ui->labelName->setText(technology.getName());
 }
