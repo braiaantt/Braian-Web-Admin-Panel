5f6dcad017c2b33cdc84fe7ec4fb4f069ceb1296
Braian Torres <brianntorress.s@gmail.com>
2025-11-21 16:34:37 -0300

    feat(PortfolioProject): implement new constrcutor and init() method

--- custome_widgets/portfolioproject.cpp
+++ custome_widgets/portfolioproject.cpp
@@ -1,14 +1,27 @@
 #include "portfolioproject.h"
 #include "ui_portfolioproject.h"
 
-PortfolioProject::PortfolioProject(QWidget *parent)
+PortfolioProject::PortfolioProject(QWidget *parent, const Project &project)
     : QWidget(parent)
     , ui(new Ui::PortfolioProject)
 {
     ui->setupUi(this);
+    init(project);
 }
 
 PortfolioProject::~PortfolioProject()
 {
     delete ui;
+}
+
+//------ Initialization ------
+
+void PortfolioProject::init(const Project &project)
+{
+    if(!project.getCoverPath().isEmpty()){
+        ui->labelImage->setPixmap(QPixmap(project.getCoverPath()));
+    }
+
+    ui->labelTitle->setText(project.getName());
+    ui->labelSmallAbout->setText(project.getSmallAbout());
 }
