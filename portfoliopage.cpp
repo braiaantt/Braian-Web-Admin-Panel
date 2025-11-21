95f70208161cc9e933b4e78d06d16f5820d35577
Braian Torres <brianntorress.s@gmail.com>
2025-11-21 16:53:31 -0300

    refactor(PortfolioPage): use 'technology' to create a TechnologyWidget and insert it to scrollArea at clicked slot for pushButtonAddTechnology feat: implement clicked slot for pushButtonAddProject

--- portfoliopage.cpp
+++ portfoliopage.cpp
@@ -1,6 +1,9 @@
 #include "portfoliopage.h"
 #include "ui_portfoliopage.h"
 #include "createtechnology.h"
+#include "createproject.h"
+#include "portfolioproject.h"
+#include "technologywidget.h"
 
 PortfolioPage::PortfolioPage(QWidget *parent)
     : QWidget(parent)
@@ -21,10 +24,28 @@ void PortfolioPage::on_pushButtonAddTechnology_clicked()
     CreateTechnology dialog(this);
     if(dialog.exec() == QDialog::Rejected) return;
 
-    Technology *technology = dialog.getTechnology();
     QHBoxLayout *layout = (QHBoxLayout*)ui->scrollAreaTechnologyWidgetContents->layout();
+    Technology technology = dialog.getTechnology();
+    TechnologyWidget *technologyWidget = new TechnologyWidget(nullptr, technology);
+    technologyWidget->setParent(ui->scrollAreaTechnologyWidgetContents); // to avoid memory leak warning
 
-    if(technology && layout){
-        layout->insertWidget(layout->count()-1, technology);
+    if(technologyWidget && layout){
+        layout->insertWidget(layout->count()-1, technologyWidget);
     }
 }
+
+void PortfolioPage::on_pushButtonAddProject_clicked()
+{
+    CreateProject dialog(this);
+    if(dialog.exec() == QDialog::Rejected) return;
+
+    QHBoxLayout *layout = (QHBoxLayout*)ui->scrollAreaProjectWidgetContents->layout();
+    Project project = dialog.getProject();
+    PortfolioProject *projectWidget = new PortfolioProject(nullptr, project);
+    projectWidget->setParent(ui->scrollAreaProjectWidgetContents); // to avoid memory leak warning
+
+    if(projectWidget && layout){
+        layout->insertWidget(layout->count()-1, projectWidget);
+    }
+}
+
