b8fd9b810cf7929e9c7a276ca1634da1940517e1
Braian Torres <brianntorress.s@gmail.com>
2025-11-21 03:39:55 -0300

    feat(Technology): initializate values at constructor

--- custome_widgets/technology.cpp
+++ custome_widgets/technology.cpp
@@ -1,11 +1,14 @@
 #include "technology.h"
 #include "ui_technology.h"
 
-Technology::Technology(QWidget *parent, const QPixmap &image, const QString &name)
+Technology::Technology(QWidget *parent, const QPixmap &photo, const QString &name, const QString &_filePath)
     : QWidget(parent)
     , ui(new Ui::Technology)
 {
     ui->setupUi(this);
+    ui->labelPhoto->setPixmap(photo);
+    ui->labelName->setText(name);
+    filePath = _filePath;
 }
 
 Technology::~Technology()
