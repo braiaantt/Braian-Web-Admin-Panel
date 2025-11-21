5454fb7884320290adc7771fc991b016e3bcaed3
Braian Torres <brianntorress.s@gmail.com>
2025-11-21 16:35:43 -0300

    refactor(TechnologyWidget): rename class from 'Technology' to 'TechnologyWidget' and add Technology paramter to constructor. feat: add init() method

--- custome_widgets/technologywidget.h
+++ custome_widgets/technologywidget.h
@@ -1,23 +1,27 @@
-#ifndef TECHNOLOGY_H
-#define TECHNOLOGY_H
+#ifndef TECHNOLOGYWIDGET_H
+#define TECHNOLOGYWIDGET_H
 
 #include <QWidget>
+#include "technology.h"
 
 namespace Ui {
 class Technology;
 }
 
-class Technology : public QWidget
+class TechnologyWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit Technology(QWidget *parent, const QPixmap &photo, const QString &name, const QString &filePath = "");
-    ~Technology();
+    explicit TechnologyWidget(QWidget *parent = nullptr, const Technology &technology = Technology());
+    ~TechnologyWidget();
 
 private:
     Ui::Technology *ui;
-    QString filePath;
+
+    //------ Initialization ------
+    void init(const Technology &technology);
+
 };
 
-#endif // TECHNOLOGY_H
+#endif // TECHNOLOGYWIDGET_H
