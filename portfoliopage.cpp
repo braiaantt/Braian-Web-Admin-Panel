8f68681cb781f0ed53bf7f24805c3888d1d7ad9f
Braian Torres <brianntorress.s@gmail.com>
2025-11-21 03:50:05 -0300

    feat(PorfolioPage): implement clicked slot for pushButtonAddTechnology

--- portfoliopage.cpp
+++ portfoliopage.cpp
@@ -1,5 +1,6 @@
 #include "portfoliopage.h"
 #include "ui_portfoliopage.h"
+#include "createtechnology.h"
 
 PortfolioPage::PortfolioPage(QWidget *parent)
     : QWidget(parent)
@@ -11,4 +12,19 @@ PortfolioPage::PortfolioPage(QWidget *parent)
 PortfolioPage::~PortfolioPage()
 {
     delete ui;
+}
+
+//------ UI Slots ------
+
+void PortfolioPage::on_pushButtonAddTechnology_clicked()
+{
+    CreateTechnology dialog(this);
+    if(dialog.exec() == QDialog::Rejected) return;
+
+    Technology *technology = dialog.getTechnology();
+    QHBoxLayout *layout = (QHBoxLayout*)ui->scrollAreaTechnologyWidgetContents->layout();
+
+    if(technology && layout){
+        layout->insertWidget(layout->count()-1, technology);
+    }
 }
