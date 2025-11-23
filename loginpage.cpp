7093c80316edf06480fc9e40c46c7279b4dc8210
Braian Torres <brianntorress.s@gmail.com>
2025-11-23 00:41:45 -0300

    feat(LoginPage): implement connectSignalsAndsLots(), clicked slot for pushButtonLogin, loginSuccess() and error() methods

--- loginpage.cpp
+++ loginpage.cpp
@@ -1,14 +1,51 @@
 #include "loginpage.h"
 #include "ui_loginpage.h"
+#include "utils.h"
 
-LoginPage::LoginPage(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::LoginPage)
+LoginPage::LoginPage(AuthService *authService, QWidget *parent)
+    :
+    ui(new Ui::LoginPage),
+    authService(authService),
+    QWidget(parent)
 {
     ui->setupUi(this);
+    connectSignalsAndSlots();
 }
 
 LoginPage::~LoginPage()
 {
     delete ui;
+}
+
+//------ Initialization ------
+
+void LoginPage::connectSignalsAndSlots()
+{
+    connect(authService, &AuthService::loginSuccess, this, &LoginPage::loginSuccess);
+    connect(authService, &AuthService::error, this, &LoginPage::error);
+}
+
+
+//------ UI Slots ------
+
+void LoginPage::on_pushButtonLogin_clicked()
+{
+    QString email = ui->lineEditEmail->text();
+    QString password = ui->lineEditPassword->text();
+
+    authService->login(email, password);
+}
+
+//------ Private Slots ------
+
+void LoginPage::loginSuccess()
+{
+    ui->lineEditEmail->setText("");
+    ui->lineEditPassword->setText("");
+    qDebug()<<"Conectadooo breoooooooo";
+}
+
+void LoginPage::error(const QString &error)
+{
+    Utils::showWarning(this, error);
 }
