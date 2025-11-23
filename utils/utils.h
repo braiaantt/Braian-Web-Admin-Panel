444d1a6fc6ad74951baa8f67be5dfb946f9036c2
Braian Torres <brianntorress.s@gmail.com>
2025-11-23 00:38:41 -0300

    feat(utils): add showWarning() method

--- utils/utils.h
+++ utils/utils.h
@@ -7,6 +7,7 @@
 namespace Utils{
     QString selectImageFile();
     QPixmap roundedPixmap(const QPixmap &pixmap, const QSize &targetSize);
+    void showWarning(QWidget* parent, const QString &message);
 }
 
 #endif // UTILS_H
