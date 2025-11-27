ef067e2ab6477f64f53d511942434dac7c1e4b56
Braian Torres <brianntorress.s@gmail.com>
2025-11-26 23:01:53 -0300

    feat(TechnologyWidget): implement getters, setters, ui slots and config methods

--- pages/portfolio_page/custome_widgets/technologywidget.cpp
+++ pages/portfolio_page/custome_widgets/technologywidget.cpp
@@ -3,11 +3,11 @@
 #include "utils.h"
 
 TechnologyWidget::TechnologyWidget(QWidget *parent, const Technology &technology)
-    : QWidget(parent)
+    : QWidget(parent), technology(technology)
     , ui(new Ui::Technology)
 {
     ui->setupUi(this);
-    init(technology);
+    init();
 }
 
 TechnologyWidget::~TechnologyWidget()
@@ -17,12 +17,48 @@ TechnologyWidget::~TechnologyWidget()
 
 //------ Initialization ------
 
-void TechnologyWidget::init(const Technology &technology)
+void TechnologyWidget::init()
 {
-    if(!technology.getImgPath().isEmpty()){
-        QPixmap pixmap = Utils::roundedPixmap(QPixmap(technology.getImgPath()), ui->labelPhoto->size());
-        ui->labelPhoto->setPixmap(pixmap);
-    }
-
     ui->labelName->setText(technology.getName());
+    ui->pushButtonDelete->setVisible(false);
+    ui->radioButton->setVisible(false);
+}
+
+//------ Config Methods ------
+
+void TechnologyWidget::enableDeleteButton()
+{
+    ui->pushButtonDelete->setVisible(true);
+}
+
+void TechnologyWidget::enableRadioButton()
+{
+    ui->radioButton->setVisible(true);
+}
+
+//------ UI Slots ------
+
+void TechnologyWidget::on_pushButtonDelete_clicked()
+{
+    emit deleteTechnology(technology);
+}
+
+//------ Getters ------
+
+const Technology& TechnologyWidget::getTechnology() const
+{
+    return technology;
+}
+
+int TechnologyWidget::isSelected() const
+{
+    return ui->radioButton->isChecked();
+}
+
+//------ Setters ------
+
+void TechnologyWidget::setTechIcon(const QPixmap &pixmap)
+{
+    QPixmap rounded = Utils::roundedPixmap(pixmap, ui->labelPhoto->size());
+    ui->labelPhoto->setPixmap(rounded);
 }
