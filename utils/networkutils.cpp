d2f16c05a9c4bb0a57f81789d74b4a49f8cae000
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 05:54:54 -0300

    refactor(NetworkUtils): remove early reply->error() check and include errorString in fallback

--- utils/networkutils.cpp
+++ utils/networkutils.cpp
@@ -6,11 +6,6 @@ bool NetworkUtils::checkError(QNetworkReply *reply, QString &errorMsg)
 {
     int status = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
 
-    if(reply->error() != QNetworkReply::NoError){
-        errorMsg = reply->errorString();
-        return false;
-    }
-
     if(status >= 200 && status < 300)
         return true;
 
@@ -20,7 +15,7 @@ bool NetworkUtils::checkError(QNetworkReply *reply, QString &errorMsg)
     if(doc.isObject() && doc.object().contains("detail")){
         errorMsg = doc.object().value("detail").toString();
     } else {
-        errorMsg = QString("HTTP %1 Error").arg(status);
+        errorMsg = QString("HTTP %1 Error: %2").arg(status).arg(reply->errorString());
     }
 
     return false;
