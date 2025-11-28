b4a26ac9e154caea56835e1f4b75be46bc7e9ee0
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 19:06:21 -0300

    refactor(TechnologiesView): initializate 'layout' at constructor and remove layout casting from all methods

--- pages/portfolio_page/custome_widgets/technologiesview.cpp
+++ pages/portfolio_page/custome_widgets/technologiesview.cpp
@@ -1,22 +1,18 @@
 #include "technologiesview.h"
-#include <QHBoxLayout>
 
 TechnologiesView::TechnologiesView(QWidget *parent) :
     QScrollArea(parent)
 {
-
+    layout = qobject_cast<QHBoxLayout*>(this->widget()->layout());
 }
 
 void TechnologiesView::addTechnologyWidget(TechnologyWidget *widget)
 {
-    QHBoxLayout *layout = qobject_cast<QHBoxLayout*>(this->widget()->layout());
     layout->insertWidget(layout->count() - 1, widget);
 }
 
 void TechnologiesView::deleteTechnologyWidget(int id)
 {
-    QHBoxLayout *layout = qobject_cast<QHBoxLayout*>(this->widget()->layout());
-
     for(int i = 0; i<layout->count(); i++){
         TechnologyWidget *techWidget = fromWidget(layout->itemAt(i)->widget());
         if(!techWidget) continue;
@@ -31,8 +27,6 @@ void TechnologiesView::deleteTechnologyWidget(int id)
 
 void TechnologiesView::setTechIcon(int techId, const QPixmap &icon)
 {
-    QHBoxLayout *layout = qobject_cast<QHBoxLayout*>(this->widget()->layout());
-
     for(int i = 0; i<layout->count(); i++){
         TechnologyWidget *techWidget = fromWidget(layout->itemAt(i)->widget());
         if(!techWidget) continue;
