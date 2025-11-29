4ea32590903170686d59cffc49c9f9fd841250ef
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 23:56:32 -0300

    feat(TechnologiesView): implement init(), selectTechnology(), getInitialStates() and getFInalStates() methods. Set values to initialStates at addTechnologyWidget() method

--- pages/portfolio_page/custome_widgets/technologiesview.cpp
+++ pages/portfolio_page/custome_widgets/technologiesview.cpp
@@ -3,12 +3,22 @@
 TechnologiesView::TechnologiesView(QWidget *parent) :
     QScrollArea(parent)
 {
+
+}
+
+//------ Initialization ------
+
+void TechnologiesView::init()
+{
     layout = qobject_cast<QHBoxLayout*>(this->widget()->layout());
 }
+
+//------ Public Methods ------
 
 void TechnologiesView::addTechnologyWidget(TechnologyWidget *widget)
 {
     layout->insertWidget(layout->count() - 1, widget);
+    initialStates.insert(widget->getTechnology().getId(), false);
 }
 
 void TechnologiesView::deleteTechnologyWidget(int id)
@@ -36,6 +46,43 @@ void TechnologiesView::setTechIcon(int techId, const QPixmap &icon)
             break;
         }
     }
+}
+
+void TechnologiesView::selectTechnology(int techId)
+{
+    for(int i = 0; i<layout->count(); i++){
+        TechnologyWidget *techWidget = fromWidget(layout->itemAt(i)->widget());
+        if(!techWidget) continue;
+
+        if(techWidget->getTechnology().getId() == techId){
+            techWidget->setSelected(true);
+            initialStates[techId] = true;
+            break;
+        }
+    }
+}
+
+//------ Getters ------
+
+QHash<int, bool> TechnologiesView::getInitialStates() const
+{
+    return initialStates;
+}
+
+QHash<int, bool> TechnologiesView::getFinalStates()
+{
+    QHash<int, bool> finalStates;
+
+    for(int i = 0; i<layout->count(); i++){
+        TechnologyWidget *techWidget = fromWidget(layout->itemAt(i)->widget());
+        if(!techWidget) continue;
+
+        int id = techWidget->getTechnology().getId();
+        bool state = techWidget->isSelected();
+        finalStates.insert(id, state);
+    }
+
+    return finalStates;
 }
 
 //------ Helpers ------
