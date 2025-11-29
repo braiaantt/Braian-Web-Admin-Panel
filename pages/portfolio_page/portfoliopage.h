cfbe394d538d243638d876dc09d6b512023de8d5
Braian Torres <brianntorress.s@gmail.com>
2025-11-29 12:46:52 -0300

    feat(PortfolioPage): inyect ProjectService, add projectCoverReceipt() private slot and setProjectWidgets() helper method

--- pages/portfolio_page/portfoliopage.h
+++ pages/portfolio_page/portfoliopage.h
@@ -5,6 +5,7 @@
 #include "portfolioservice.h"
 #include "technologyservice.h"
 #include "entitytechservice.h"
+#include "projectservice.h"
 #include "portfolio.h"
 
 namespace Ui {
@@ -16,7 +17,8 @@ class PortfolioPage : public QWidget
     Q_OBJECT
 
 public:
-    explicit PortfolioPage(PortfolioService *portfolioService, TechnologyService *technologyService, EntityTechService *entityTechService,
+    explicit PortfolioPage(PortfolioService *portfolioService, TechnologyService *technologyService,
+                           EntityTechService *entityTechService, ProjectService *projectService,
                            QWidget *parent = nullptr);
     ~PortfolioPage();
 
@@ -27,6 +29,7 @@ private slots:
     void setUserPhoto(const QPixmap &pixmap);
     void techIconReceipt(int techId, const QPixmap &pixmap);
     void refreshTechnologies();
+    void projectCoverReceipt(int projectId, const QPixmap &pixmap);
     void errorOcurred(const QString &message);
 
     //------ UI Slots ------
@@ -43,12 +46,14 @@ private:
     PortfolioService *portfolioService;
     TechnologyService *technologyService;
     EntityTechService *entityTechService;
+    ProjectService *projectService;
 
     //------ Initialization ------
     void connectSignalsAndSlots();
 
     //------ Helpers ------
     void setTechnologyWidgets(const QVector<Technology> &techs);
+    void setProjectWidgets(const QVector<Project> &projects);
 };
 
 #endif // PORTFOLIOPAGE_H
