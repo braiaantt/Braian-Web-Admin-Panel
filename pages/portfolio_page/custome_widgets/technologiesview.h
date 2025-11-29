44390bc79bb13e05abdb7166d546ba8039046aa1
Braian Torres <brianntorress.s@gmail.com>
2025-11-28 23:55:10 -0300

    feat(TechnologiesView): add init(), selectTechnology() getInitialStates() and getFinalStates() public methods. Add initialStates private attribute

--- pages/portfolio_page/custome_widgets/technologiesview.h
+++ pages/portfolio_page/custome_widgets/technologiesview.h
@@ -10,12 +10,22 @@ class TechnologiesView : public QScrollArea
 public:
     TechnologiesView(QWidget *parent = nullptr);
 
+    //------ Initialization ------
+    void init();
+
+    //------ Public Methods ------
     void addTechnologyWidget(TechnologyWidget *widget);
     void deleteTechnologyWidget(int id);
     void setTechIcon(int techId, const QPixmap &pixmap);
+    void selectTechnology(int techId);
+
+    //------ Getters ------
+    QHash<int, bool> getInitialStates() const;
+    QHash<int, bool> getFinalStates();
 
 private:
     QHBoxLayout *layout;
+    QHash<int, bool> initialStates;
 
     //------ Helpers ------
     TechnologyWidget* fromWidget(QWidget *widget);
