8ceb68161373e7b47422350f12ced83fa2f1a6cc
Braian Torres <brianntorress.s@gmail.com>
2025-12-03 09:56:51 -0300

    feat(PortfolioService): add updateUserPhoto(), userPhotoUpdated() updateUserPhotoFinished(), handleUpdateUserPhoto and makeUserPhotoMultiPart(). refactor: move getUserPhoto() to public

--- services/portfolioservice.h
+++ services/portfolioservice.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QJsonArray>
+#include <QHttpMultiPart>
 #include "apiclient.h"
 #include "portfolio.h"
 
@@ -13,26 +14,29 @@ public:
     PortfolioService(ApiClient *apiClient, QObject *parent = nullptr);
 
     void getPortfolio();
+    void getUserPhoto(const QString &path);
+    void updateUserPhoto(int portfolioId, const QString &srcPath);
 
 signals:
     void portfolioReceipt(const Portfolio &portfolio);
     void userPhotoReceipt(const QPixmap& photo);
+    void userPhotoUpdated(const QString &path);
     void errorOcurred(const QString &message);
 
 private slots:
     void getPortfolioFinished();
+    void updateUserPhotoFinished();
     void getUserPhotoFinished();
 
 private:
     ApiClient *apiClient;
 
-    //------ Methods ------
-    void getUserPhoto(const QString &path);
-
     //------ Request Handlers ------
     Portfolio handlePortfolioFinished(const QByteArray &data);
+    QString handleUpdateUserPhoto(const QByteArray &data);
 
     //------ Helpers ------
+    QHttpMultiPart* makeUserPhotoMultiPart(const QString &srcPath);
     QVector<Technology> getTechnologiesFromArray(const QJsonArray &array);
     QVector<Project> getProjectsFromArray(const QJsonArray &array);
 };
